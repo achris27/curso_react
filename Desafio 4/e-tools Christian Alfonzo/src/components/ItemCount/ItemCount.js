235630d0e7f214fd6e94ac2d86f2570861fbd40e
achris27 <christianderm637@gmail.com>
2022-06-16 22:39:16 -0300

    desafio entregable numero 4 corregido

--- Desafio 4/e-tools Christian Alfonzo/src/components/ItemCount/ItemCount.js
+++ Desafio 4/e-tools Christian Alfonzo/src/components/ItemCount/ItemCount.js
@@ -1,7 +1,7 @@
 import "./ItemCount.css";
 import { useState } from "react";
 
-const ItemCount = ({stock, inicial}) => {
+const ItemCount = ({stock, inicial, onAdd}) => {
 
     inicial = parseInt(inicial);
     let [number, setNumber] = useState(inicial);
@@ -17,21 +17,16 @@ const ItemCount = ({stock, inicial}) => {
             setNumber(number - 1);
         }
     };
-
-    const button_kart = () => {
-        alert("Se sumó al carrito la cantidad de: " + number);
-        setNumber(number = inicial)
-    }
-
-    return(
-        <div className="App">
-            <input type="number" value={number} className="item_cant"></input>
-            <br></br>
-            <button onClick={button_minus} className="button_item">restar</button>
-            <button onClick={button_kart} className="button_item">Sumar al carrito</button>
-            <button onClick={button_plus} className="button_item">sumar</button>
-        </div>
-    );
+    
+        return(
+            <div className="App">
+                <input type="number" value={number} className="item_cant"></input>
+                <br></br>
+                <button onClick={button_minus} className="button_item">restar</button>
+                <button onClick={() => onAdd(number)} className="button_item">Sumar al carrito</button>
+                <button onClick={button_plus} className="button_item">sumar</button>
+            </div>
+        );
 };
 
 export default ItemCount;
--- Desafio 4/e-tools Christian Alfonzo/src/components/ItemListContainer/ItemListContainer.js
+++ Desafio 4/e-tools Christian Alfonzo/src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,16 @@
 import ItemCount from '../ItemCount/ItemCount'
 
+const initial = 1;
+
 const ItemListContainer = ({greeting}) => {
+  function onAdd(number) {
+    console.log("Se sumó al carrito la cantidad de: " + number);
+  }
+  
     return(
       <div className="App">
         <p>{greeting}</p>
-        <ItemCount stock="10" inicial="1"/>
+        <ItemCount stock="10" inicial={initial} onAdd={onAdd}/>
       </div>
     );
 };
