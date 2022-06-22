7d1f6195130ecedba1cd7846975ee5cb4aeebb31
achris27 <christianderm637@gmail.com>
2022-06-21 21:39:49 -0300

    desafio entregable numero 5 corregido

--- Desafio 5/e-tools Christian Alfonzo/src/components/ItemList/ItemList.js
+++ Desafio 5/e-tools Christian Alfonzo/src/components/ItemList/ItemList.js
@@ -1,24 +1,15 @@
 import Item from '../Item/Item'
 import './ItemList.css'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-const ItemList = () => {
-    const [products, setProducts] = useState([]);
-
-
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then((response) => response.json())
-            .then((json) => setProducts((json)));
-    }, []);
-    console.log('DATA: ', products)
+const ItemList = (datas) => {
 
   return (
     <div>
         <h1>Products</h1>
         <div className='Items'>
-            {products.map((product) => (
-                <Item key={product.id} data={product}/>
+            {datas.data.map((data) => (
+                <Item key={data.id} data={data}/>
             ))}
         </div>
     </div>
--- Desafio 5/e-tools Christian Alfonzo/src/components/ItemListContainer/ItemListContainer.js
+++ Desafio 5/e-tools Christian Alfonzo/src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,23 @@
 import ItemList from "../ItemList/ItemList";
 import './ItemListContainer.css'
+import React, { useState, useEffect } from 'react'
+
 
 const ItemListContainer = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then((response) => response.json())
+            .then((json) => setProducts((json)));
+    }, []);
+
+    //console.log('DATA: ', products)
+
     return(
       <div className="ItemListContainer">
-        <ItemList />
+        <ItemList data={products}/>
       </div>
     );
 };
